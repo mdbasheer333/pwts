a72ea5fbacad15a0951de03fe85d38c99cde7f99
basheer mohammad <basheer.m.mohammad@oracle.com>
2026-06-23 21:46:37 +0530

    vidoe/retry options

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 0 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -29,8 +29,8 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    video: 'on', 
-    trace: 'on',
+    video: 'retain-on-failure', 
+    trace: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
